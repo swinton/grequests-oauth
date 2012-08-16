8e22feda87837c5db88fd6b764518ff184c7f03d
Steve Winton <stevewinton@gmail.com>
2012-08-16 18:13:40 +0100

    Bugfix in grequests_oauth.py: was patching grequests, not client

--- grequests_oauth.py
+++ grequests_oauth.py
@@ -8,20 +8,22 @@ import grequests, requests
 
 from oauth_hook import OAuthHook
 
-def grequests_oauth_client(access_token=None, access_token_secret=None, consumer_key=None, consumer_secret=None, header_auth=None):
+def grequests_oauth_client(access_token=None, access_token_secret=None, consumer_key=None, consumer_secret=None, header_auth=None, **kwargs):
     oauth_hook = OAuthHook(access_token=access_token, access_token_secret=access_token_secret, 
                            consumer_key=consumer_key, consumer_secret=consumer_secret, 
                            header_auth=header_auth)
 
+    # TODO: support other hooks passed in via kwargs
+
     client = requests.session(hooks={'pre_request': oauth_hook})
 
     client.get = grequests.patched(client.get)
-    client.options = grequests.patched(grequests.options)
-    client.head = grequests.patched(grequests.head)
-    client.post = grequests.patched(grequests.post)
-    client.put = grequests.patched(grequests.put)
-    client.patch = grequests.patched(grequests.patch)
-    client.delete = grequests.patched(grequests.delete)
-    client.request = grequests.patched(grequests.request)
+    client.options = grequests.patched(client.options)
+    client.head = grequests.patched(client.head)
+    client.post = grequests.patched(client.post)
+    client.put = grequests.patched(client.put)
+    client.patch = grequests.patched(client.patch)
+    client.delete = grequests.patched(client.delete)
+    client.request = grequests.patched(client.request)
 
     return client
